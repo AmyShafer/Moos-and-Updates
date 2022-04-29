db42fb60ef8ad33e689fc70caec0403b513edf47
Amy Shafer <amy.u.shafer@gmail.com>
2022-04-29 15:24:17 -0400

    data file

---
+++ utils/data.js
@@ -0,0 +1,101 @@
+const names = [
+    "Colin",
+    "Woody",
+    "Jo",
+    "Johnny",
+    "Maribeth",
+    "Maybelle",
+    "Juniper",
+    "Mikey",
+    "Maximus",
+    "Judy",
+    "Penny",
+    "Stanley",
+    "Olive",
+    "Amelia",
+    "Antonio",
+    "Curly",
+    "Momma",
+    "Ari",
+    "Hurley",
+    "Marsha",
+    "Hurley",
+    "Leroy",
+    "Oscar",
+    "Dino",
+    "Lola",
+    "Bosley",
+    "Charlie",
+    "Molly",
+    "Bunny"
+]
+
+const animals = [
+    "cow",
+    "pig",
+    "goat",
+    "chicken",
+    "turkey",
+    "bunny",
+    "duck",
+    "geese",
+    "dog",
+    "cat"
+];
+
+const thoughts = [
+    "The pasture is breeze couldn\'t be more perfect",
+    "Moo!",
+    "This hay hits the spot!",
+    "Rain\'s coming! Let me lay down.",
+    "I like this person...I think I\'ll try for a neck scratch!",
+    "That\'s the spot! Keep scratching!",
+    "What a day to be alive!",
+    "Treats! I love treats!",
+    "This is some high quality grass right here!",
+    "Mud, mud! I love mud!",
+    "Human, rub my belly!",
+    "What\'s this?",
+    "Time to clean my pen!",
+    "Snow\'s back!",
+    "Fresh fruit, hooray!",
+    "This person is my new favorite person!",
+    "Someone new is here!"
+];
+
+// get a random element given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Get a random user name
+const getUserName = () => {
+    `${getRandomArrItem(names)}`
+};
+
+// Get email
+const getEmail = () => {
+    `${getRandomArrItem(animals)}@woodstock.org`
+};
+
+// Get thoughts
+const getThoughts = (int) => {
+    const results = [];
+    for (let i = 0; i < int; i++) {
+        results.push({
+            newThought: getRandomArrItem(thoughts)
+        });
+    }
+    return results;
+};
+
+// Get friends
+const getFriends = () => {
+
+};
+
+// export the function for use in seed.js
+module.exports = {
+    getUserName,
+    getEmail,
+    getThoughts,
+    getFriends
+};
