48da6636a1d86d81b3d62a042019384d42892095
Amy Shafer <amy.u.shafer@gmail.com>
2022-05-02 22:35:44 -0400

    working on connection file error

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,8 @@
-const {
-    connect,
-    connection
-} = require('mongoose');
+const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:3001/woodstock';
-
-connect(connectionString, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/woodstock', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-module.exports = connection;
+module.exports = mongoose.connection;
