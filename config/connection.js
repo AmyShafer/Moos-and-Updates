ae4e666a49766d8b66250aa32b9bdc069174e336
Amy Shafer <amy.u.shafer@gmail.com>
2022-05-13 01:06:49 -0400

    route correction

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/woodstock', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/woodstock', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
